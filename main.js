d40edaea79eeb44d11f26e2193f625a61c8e0f7f
Dosyaan <dastanmatraimov79@gmail.com>
2024-10-28 17:48:10 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+// task - 1
+
+// const num = [1,2,3]
+// const numMap = num.map((item =>{
+//     return item * 2;
+// }))
+
+// console.log(numMap)
+
+// task - 2
+
+// const world = ['hello', 'world']
+// const worldMap = world.map((item =>{
+//     return item.toUpperCase();
+// }))
+// console.log(worldMap)
+
+// task - 3
+
+// const num = [1,2,3,4]
+// const numMap = num.map((item =>{
+//     return item+''
+// }))
+// console.log(numMap)
+
+// task - 4
+
+// const animals = ['кот', 'собака', "тигр"]
+// const animalsMap = animals.map((item=>{
+//     return item+'!'
+// }))
+// console.log(animalsMap)
+
+// task - 5
+
+// const users = [
+//   { name: "Alice", role: "admin" },
+//   { name: "Bob", role: "user" },
+//   { name: "Charlie", role: "admin" },
+//   { name: "Dave", role: "user" }
+// ];
+
+// const usersMap = users.map((item =>{
+//     return item.name
+// }))
+// console.log(usersMap)
+
